56e0ee41eb4a343b4c1e56fcd7f1dbf67537bd47
Hare Krishna <128279576+tushardhab@users.noreply.github.com>
2024-06-23 00:33:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001678918073"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Am 𝗛𝗞 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟™ 😊store bot You will get here Study Material by links which will be provided in channels.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello👋🏻 {mention}\n\nI Am 𝗛𝗞 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟™ 😊store bot You will get here Study Material by links🔗 which will be provided in channels✅⭐️.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "6155478725").split()):
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You need to join in my official channel😊 to get lectures\n\nKindly Please join Channel\n\n And Click on try again or go on post link again\n\nThankyou\n\n𝗛𝗞 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟™~</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello👋🏻 {mention}\n\n<b>⚠️You need to join in my official channel😊 to get lectures📹\n➡️Kindly Please join Channel✅‼️\n⭕️And Click on try again or go on post link again✅⭐️\nThankyou😊\n⭐️𝗛𝗞 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟™~</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" el
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Bhratashree 😊!!\n\nAap humare owner nahi hai kripya yaha msg naa kre ye bot sirf owner ki sunta hai kuch puchna hai to iss username pe msg kre @THEHEATHERS_BOT\n\nDhanyawad!"
+USER_REPLY_TEXT = "❌Bhratashree 😊!!\n\n➡️Aap humare owner nahi hai kripya yaha msg naa kre ye bot sirf owner ki sunta hai kuch puchna hai to iss username pe msg kre @THEHEATHERS_BOT 🙏\n\nDhanyawad!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6155478725)
