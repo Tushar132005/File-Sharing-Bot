87dbf43df4c96e344beba8e3b09541a97e12032d
Tushar132005 <successspark09@gmail.com>
2024-07-12 13:05:06 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,8 +20,7 @@ from utils import verify_user, check_token, check_verification, get_token
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
-async def start_command(client: Client, message: Message):
-     client = bot
+async def start_command(client: Client, message: Message)
      data = message.command[1]
      if data.split("-", 1)[0] == "verify": # set if or elif it depend on your code
         userid = data.split("-", 2)[1]
