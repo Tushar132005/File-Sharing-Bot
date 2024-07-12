1c970c13504d6dab55479fef22e36f38b6395650
Tushar132005 <successspark09@gmail.com>
2024-07-12 13:33:19 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,6 +21,27 @@ from utils import verify_user, check_token, check_verification, get_token
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message)
+data = message.command[1]
+    if data.split("-", 1)[0] == "verify": # set if or elif it depend on your code
+        userid = data.split("-", 2)[1]
+        token = data.split("-", 3)[2]
+        if str(message.from_user.id) != str(userid):
+            return await message.reply_text(
+                text="<b>Invalid link or Expired link !</b>",
+                protect_content=True
+            )
+        is_valid = await check_token(client, userid, token)
+        if is_valid == True:
+            await message.reply_text(
+                text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all files till today midnight.</b>",
+                protect_content=True
+            )
+            await verify_user(client, userid, token)
+        else:
+            return await message.reply_text(
+                text="<b>Invalid link or Expired link !</b>",
+                protect_content=True
+            )
 #othercode#
     id = message.from_user.id
     if not await present_user(id):
@@ -53,7 +74,11 @@ async def start_command(client: Client, message: Message)
                     if i < end:
                         break
         elif len(argument) == 2:
-        if not await check_verification(client, message.from_user.id) and VERIFY == True:
+            try:
+                ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+            except:
+                return
+if not await check_verification(client, message.from_user.id) and VERIFY == True:
             btn = [[
                 InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{BOT_USERNAME}?start="))
             ],[
@@ -65,10 +90,6 @@ async def start_command(client: Client, message: Message)
                 reply_markup=InlineKeyboardMarkup(btn)
             )
             return
-            try:
-                ids = [int(int(argument[1]) / abs(client.db_channel.id))]
-            except:
-                return
         temp_msg = await message.reply("Please wait...")
         try:
             messages = await get_messages(client, ids)
@@ -120,29 +141,7 @@ async def start_command(client: Client, message: Message)
             quote = True
         )
         return
- data = message.command[1]
-    if data.split("-", 1)[0] == "verify": # set if or elif it depend on your code
-        userid = data.split("-", 2)[1]
-        token = data.split("-", 3)[2]
-        if str(message.from_user.id) != str(userid):
-            return await message.reply_text(
-                text="<b>Invalid link or Expired link !</b>",
-                protect_content=True
-            )
-        is_valid = await check_token(client, userid, token)
-        if is_valid == True:
-            await message.reply_text(
-                text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all files till today midnight.</b>",
-                protect_content=True
-            )
-            await verify_user(client, userid, token)
-        else:
-            return await message.reply_text(
-                text="<b>Invalid link or Expired link !</b>",
-                protect_content=True
-            )
 
-    
 #=====================================================================================##
 
 WAIT_MSG = """"<b>😊 कृपया प्रतीक्षा करें।</b>"""
